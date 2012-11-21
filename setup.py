db977ec17417ab929537834c23daddf38993e252
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-21 23:20:03 +0200

    Release 0.4.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (
 
 setup(
     name='collective.cart.shipping',
-    version='0.4.1.1',
+    version='0.4.1.2',
     description="Adds shipping methods to Plone.",
     long_description=long_description,
     # Get more strings from
